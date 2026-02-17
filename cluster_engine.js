7f483a09db9eca07785878111bda22f91fc028ba
seonwoo7170-tech <seonwoo7170@gmail.com>
2026-02-17 21:04:21 +0900

    Update cluster_engine.js via Studio VUE Magic Setup

--- cluster_engine.js
+++ cluster_engine.js
@@ -97,7 +97,8 @@ async function run() {
     console.log("📡 Connecting to Blogger API...");
     const oauth2Client = new google.auth.OAuth2(
         process.env.GOOGLE_CLIENT_ID,
-        process.env.GOOGLE_CLIENT_SECRET
+        process.env.GOOGLE_CLIENT_SECRET,
+        'https://developers.google.com/oauthplayground'
     );
     oauth2Client.setCredentials({ refresh_token: process.env.GOOGLE_REFRESH_TOKEN });
     const blogger = google.blogger({ version: 'v3', auth: oauth2Client });
